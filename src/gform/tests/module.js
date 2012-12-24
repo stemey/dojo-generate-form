fd7fff36116b3d94820b2a360db15cc5b5b06035
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2012-12-24 08:18:03 +0100

    renamed test group

--- src/gform/tests/module.js
+++ src/gform/tests/module.js
@@ -2,8 +2,8 @@ dojo.provide("gform.tests.module");
 
 
 try {
-	doh.registerUrl("gform.tests.Editor", "/gform/tests/test_embedded_list.html"); 
-	doh.registerUrl("gform.tests.Editor", "/gform/tests/test_embedded_polymorphic.html");
+	doh.registerUrl("gform.tests.embedded_list", "/gform/tests/test_embedded_list.html"); 
+	doh.registerUrl("gform.tests.embedded_polymorphic", "/gform/tests/test_embedded_polymorphic.html");
 }catch(e){
 	doh.debug(e);
 }
