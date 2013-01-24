88c857265be6f765fbe79532653714e0a8b79444
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2013-01-24 09:35:25 +0100

    fixed tests

--- embedded/GroupPanelWidget.js
+++ embedded/GroupPanelWidget.js
@@ -4,10 +4,10 @@ define([ "dojo/_base/array", //
 		"dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin",
 		"dojo/text!./polymorphic_embedded_attribute.html",
 		"dijit/layout/StackContainer", "dojo/Stateful",
-		"gform/Editor","../getStateful"//
+		"gform/Editor","../getStateful","../copyProperties","../mergeProperties"//
 ], function(array, lang, declare, _WidgetBase, _Container, _TemplatedMixin,
 		_WidgetsInTemplateMixin, template, StackContainer, Stateful,
-		Editor,getStateful) {
+		Editor,getStateful,copyProperties,mergeProperties) {
 
 	return declare("app.GroupPanelWidget", [ _WidgetBase, _Container,
 			_TemplatedMixin, _WidgetsInTemplateMixin ], {
@@ -54,12 +54,16 @@ define([ "dojo/_base/array", //
 			this.typeToGroup = {};
 			var me=this;
 			var typeToModel={};
-			var cloned = copyProperties(modelHandle.get(attribute.code),new Stateful({}));
+			var cloned = new Stateful({});
+			copyProperties(modelHandle.get(attribute.code),cloned);
 			typeToModel[currentType]=cloned;	
 			array.forEach(attribute.validTypes, function(type) {
 				if (type.code!=currentType) {	
 					typeToModel[type.code]=getStateful({});
 					typeToModel[type.code][attribute.type_property]=getStateful(type.code);
+					array.forEach(type.attributes,function(attribute) {
+						typeToModel[type.code][attribute.code]=null;
+					});
 				}
 				var editor = new Editor(
 					{
@@ -70,8 +74,8 @@ define([ "dojo/_base/array", //
 				this.typeToGroup[type.code] = editor;
 			}, this);
 			
-			panelModel.watch("type", function(e) {
-				var type = panelModel.get("type");
+			panelModel.watch("type", function(propName,oldValue,newValue) {
+				var type = newValue;
 				var modelHandle=me.get("modelHandle");
 				if (type == "null") {
 					modelHandle.set(attribute.code, null);
@@ -79,10 +83,17 @@ define([ "dojo/_base/array", //
 					me.typeStack.domNode.style.display="none";
 				} else {
 					me.typeStack.domNode.style.display="block";
-					if (e.oldValue!="null") {
-						copyProperties(modelHandle.get(attribute.code),typeToModel[e.oldValue])
+					if (oldValue!=newValue) {	
+						if (modelHandle.get(attribute.code)==null) {
+							var cloned = new Stateful({});
+							copyProperties(modelHandle.get(attribute.code),cloned);
+							modelHandle[attribute.code]=cloned;
+						}else{
+							copyProperties(modelHandle.get(attribute.code),typeToModel[oldValue])
+							mergeProperties(typeToModel[type],modelHandle.get(attribute.code) );
+							modelHandle[attribute.code][attribute.type_property]=getStateful(newValue);
+						}
 					}
-					mergeProperties(typeToModel[type],modelHandle.get(attribute.code) );
 					me.typeStack.selectChild(me.typeToGroup[type]);
 				}
 			});
