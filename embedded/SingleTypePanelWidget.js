d23ac902b5e6814550d9c43e6d6062a337f48aea
milltree <marion.m-hb03@web.de>
2013-02-11 14:51:50 +0100

    Fix initialization bug for NULL value in an embedded field group.

--- embedded/SingleTypePanelWidget.js
+++ embedded/SingleTypePanelWidget.js
@@ -5,26 +5,34 @@ define([ "dojo/_base/array", //
 		"dojo/text!./singletype_embedded_attribute.html",
 		"dijit/layout/StackContainer", "dojo/Stateful","../getStateful", "../Editor","../group/_GroupMixin"
 ], function(array, lang, declare, _WidgetBase, _Container, _TemplatedMixin,
-		_WidgetsInTemplateMixin, template, StackContainer, Stateful,getStateful,
-		Editor,_GroupMixin) {
+		_WidgetsInTemplateMixin, template, StackContainer, Stateful, getStateful,
+		Editor, _GroupMixin) {
 
 	return declare("app.SingleTypePanelWidget", [ _WidgetBase, _Container,
-			_TemplatedMixin, _WidgetsInTemplateMixin,_GroupMixin ], {
+			_TemplatedMixin, _WidgetsInTemplateMixin, _GroupMixin ], {
+		
 		templateString : template,
-		//_setMetaAttr : function(/* dojo/Stateful */attribute) {
+		
 		postCreate : function() {
 			var attribute=this.get("meta");
 			this.panelModel = new dojo.Stateful();
 			this.panelModel.set("empty", false);
 			this.panelModel.set("title", "");
+			
 			var modelHandle = this.get("modelHandle");
-			modelHandle.saveModel=modelHandle.value
+			if (modelHandle.value==null) {
+				modelHandle.value=new Stateful();
+				this.panelModel.set("empty", true);
+				this.containerNode.style.display="none";
+			}
+			modelHandle.saveModel=modelHandle.value;
 			
 			this.editor = new Editor({"modelHandle": modelHandle,"meta": attribute.validTypes[0],editorFactory:this.editorFactory});
 			this.panelModel.watch("empty", lang.hitch(this,"switchedNull"));
 			this.addChild(this.editor);
 			this.set("target", this.panelModel);
 		},
+		
 		getChildrenToValidate:function() {
 			if (this.panelModel.get("empty")) {
 				return [];
@@ -32,18 +40,22 @@ define([ "dojo/_base/array", //
 				return this.inherited(arguments);
 			}
 		}, 
+		
 		switchedNull: function() {
-				var modelHandle=this.get("modelHandle");
-				if (this.panelModel.get("empty")) {
-					modelHandle.savedValue=modelHandle.value;
-					modelHandle.set("value", null);
-					this.containerNode.style.display="none";
-					this.validateAndFire();
-				} else {
-					this.containerNode.style.display="";
-					modelHandle.set("value", modelHandle.savedValue);
-					this.validateAndFire();
-				}
+			var modelHandle=this.get("modelHandle");
+			
+			if (this.panelModel.get("empty")) {
+				modelHandle.saveModel=modelHandle.value;
+				modelHandle.set("value", null);
+				
+				this.containerNode.style.display="none";
+				this.validateAndFire();
+			} else {
+				modelHandle.set("value", modelHandle.saveModel);
+				
+				this.containerNode.style.display="";
+				this.validateAndFire();
+			}
 		}	
 
 	});
