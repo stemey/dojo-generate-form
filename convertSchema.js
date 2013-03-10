1d5faaf965e03ac561658116ea5f8292356f66c0
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2013-03-10 07:20:36 +0100

    fixed ref resolution. editor now works with circular referenes.

--- convertSchema.js
+++ convertSchema.js
@@ -1,8 +1,8 @@
 define([ "dojo/_base/array", //
 "dojo/_base/lang",//
 "dojo/_base/declare",//
-"dojox/json/ref",
-], function(array, lang, declare, ref) {
+"gform/schema/refresolve",
+], function(array, lang, declare, refresolve) {
 
   var formatToTypeMapping= {
 		date:"date",
@@ -17,7 +17,7 @@ define([ "dojo/_base/array", //
 				attribute[propTarget]=value;
 			}
 		},
-		convertAttribute: function(prop,attribute) {
+		convertAttribute: function(prop,attribute,converted) {
 			
 			//attribute.type=prop.type;
 			this.copy("description","description",prop,attribute);
@@ -33,7 +33,7 @@ define([ "dojo/_base/array", //
 				}
 			}
 			if (prop.type=="object") {
-				var types = this.convertObjectProp(prop);
+				var types = this.convertObjectProp(prop,converted);
 				attribute.type="object";
 				attribute.type_property="ext_type";
 				attribute.validTypes=types;
@@ -42,7 +42,7 @@ define([ "dojo/_base/array", //
 				if (prop.items.type=="object") {
 					attribute.type="object";
 					attribute.type_property="ext_type";
-					var types = this.convertObjectProp(prop.items);
+					var types = this.convertObjectProp(prop.items,converted);
 					attribute.validTypes=types;
 				} else if (prop.items.type=="array") {
 					throw new Error("cannot convert array of arrays");
@@ -54,23 +54,33 @@ define([ "dojo/_base/array", //
 				attribute.type=type || prop.type;
 			}
 		},
-		convertObjectProp: function(prop) {
+		convertObjectProp: function(prop,converted) {
 				if (prop.oneOf) {
-					return this.convertTypes(prop.oneOf);
+					return this.convertTypes(prop.oneOf,converted);
 				}else	if (prop.properties) {
-					return [this.convert(prop)];
+					return [this.convert(prop,converted)];
 				}else{
 					throw new Error("cannot only convert objects with oneOf");
 				}
 		},
-		convertTypes: function(schemas) {
-			return array.map(schemas,lang.hitch(this,"convert"),this);
+		convertTypes: function(schemas,converted) {
+			return array.map(schemas,function(schema) {
+				return this.convert(schema,converted);
+			},this);
 		},
-		convert: function(schema,meta) {
-			schema=ref.resolveJson(schema);
-			meta={};
+		convert: function(schema,converted,/**params*/meta) {
+			//schema=ref.resolveJson(schema);
+			//refresolve(schema);
+			converted=converted||{};
+			meta=converted[schema.id];
+			if (meta) {
+				return meta;
+			}else{
+				meta={};
+			}
 			meta.attributes=[];
 			meta.code=schema.id;
+			converted[schema.id]=meta;
 			//return  meta;
 			for (var key in schema.properties) {
 				// "__parent" is added by dojox/json/ref
@@ -79,7 +89,7 @@ define([ "dojo/_base/array", //
 					attribute.code=key;
 					var prop = schema.properties[key];
 					meta.attributes.push(attribute);
-					this.convertAttribute(prop,attribute);
+					this.convertAttribute(prop,attribute,converted);
 				}
 			}		
 			return meta;
