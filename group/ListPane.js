ae026d5e185424bb7dff3769759167bdf64b98f9
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2013-04-09 17:32:30 +0200

    dont layout a single attribute. It is a LayoutContainer but it does not resize to dims

--- group/ListPane.js
+++ group/ListPane.js
@@ -2,6 +2,10 @@ define([ "dojo/_base/declare", "dojo/_base/lang", "dijit/layout/ContentPane", ".
 ], function(declare, lang, ContentPane,_GroupMixin) {
 
 	return declare("gform.ListPane",[ ContentPane,_GroupMixin ],{
+			_checkIfSingleChild: function() {
+				// the single child maybe a layoutContainer, but it will be _LayoutMixin which only propagates resize events. 
+				this._singleChild=false;
+			}
 	});
 
 });
