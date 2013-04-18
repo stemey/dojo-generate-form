a49fb55615e3d74f45ee920b4849d66e5c362cb1
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2013-04-18 06:53:38 +0200

    improved layouting. children width is adjusted to make child layoutcontainer use the whole width

--- _LayoutMixin.js
+++ _LayoutMixin.js
@@ -13,14 +13,18 @@ define([ "dojo/_base/array", "dojo/aspect", "dojo/_base/lang", "dojo/_base/decla
 		isLayoutContainer:true,
 		resize: function(dim) {
 			this.dim=dim;
-			var widget = this.getChildren()[0];
-			if (widget.resize) {
-				if (dim) {
-					widget.resize({t:0,l:0,w:dim.w,h:dim.h});
-				} else {
-					widget.resize();
-				}
+			if (dim) {
+				this.domNode.style.width=dim.w+"px";
 			}
+			array.forEach(this.getChildren(),function(widget) {
+				if (widget.resize) {
+					if (dim) {
+						widget.resize({t:0,l:0,w:dim.w});
+					} else {
+						widget.resize();
+					}
+				}
+			});
 		}
 
 	});
--- group/ListPane.js
+++ group/ListPane.js
@@ -1,11 +1,17 @@
-define([ "dojo/_base/declare", "dojo/_base/lang", "dijit/layout/ContentPane", "./_GroupMixin"
-], function(declare, lang, ContentPane,_GroupMixin) {
+define([ "dojo/_base/declare", "dojo/_base/lang", "dojo/_base/array", "dijit/layout/ContentPane", "./_GroupMixin"
+], function(declare, lang, array, ContentPane,_GroupMixin) {
 
 	return declare("gform.ListPane",[ ContentPane,_GroupMixin ],{
-			_checkIfSingleChild: function() {
-				// the single child maybe a layoutContainer, but it will be _LayoutMixin which only propagates resize events. 
-				this._singleChild=false;
+	_layoutChildren: function(){
+		// All my child widgets are independently sized (rather than matching my size),
+		// but I still need to call resize() on each child to make it layout.
+		var cb = this._contentBox || domGeometry.getContentBox(this.containerNode);
+		array.forEach(this.getChildren(), function(widget){
+			if(widget.resize){
+				widget.resize({w: cb.w-2*cb.l});
 			}
+		});
+	},
 	});
 
 });
