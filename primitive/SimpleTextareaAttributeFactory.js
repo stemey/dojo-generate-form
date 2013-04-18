d4b71ed8a8592277600fbfbde147b382f5bb3b24
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2013-04-18 07:01:59 +0200

    added SimpleTextArea

---
+++ primitive/SimpleTextareaAttributeFactory.js
@@ -0,0 +1,45 @@
+define([ "dojo/_base/array", //
+"dojo/_base/lang",//
+"dojo/_base/declare",//
+"dojo/aspect",//
+"dojox/mvc/at",//
+"dijit/form/SimpleTextarea",//
+"../meta",//
+"./mixinTextboxBindings",
+"./dijitHelper"
+], function(array, lang, declare, aspect, at, SimpleTextarea, meta, mixinTextboxBindings, dijitHelper) {
+
+	return declare("gform.SimpleTextareaAttributeFactory", [], {
+		handles : function(attribute) {
+			return meta.isType(attribute, "string") && !attribute.array;
+		},
+		
+		create : function(attribute, modelHandle) {
+			var props = {};
+			mixinTextboxBindings(modelHandle,props);
+			dijitHelper.copyDijitProperties(attribute,props);
+			dijitHelper.copyProperty("cols", attribute, props);
+			dijitHelper.copyProperty("rows", attribute, props);
+			return  new SimpleTextarea(props);			
+		},
+		getSchema:function(){
+			var schema={};
+			schema["id"]="string";
+			schema["description"]="This is a textarea based on 'dijit.form.SimpleTextarea'";
+			schema["example"]=dojo.toJson({code:'name',type:'string'},true);
+			var properties={};
+			properties.type={type:"string",required:true,enum:["string"]};
+			properties.cols={type:"number",places:0,description:"the number of characters per line"}
+			properties.rows={type:"number",places:0,description:"the number of rows o text"}
+			dijitHelper.addSchemaProperties(properties);
+			dijitHelper.addSchemaProperty("required",properties);
+			dijitHelper.addSchemaProperty("maxLength",properties);
+			dijitHelper.addSchemaProperty("missingMessage",properties);
+			dijitHelper.addSchemaProperty("promptMessage",properties);
+			dijitHelper.addSchemaProperty("placeHolder",properties);
+
+			schema.properties=properties;
+			return schema;
+		}
+	});
+});
