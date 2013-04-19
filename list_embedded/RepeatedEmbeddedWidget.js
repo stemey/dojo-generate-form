a6d169aeb1f43388070de8a8d0d3b9e6370b7728
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2013-04-19 12:41:57 +0200

    added LayoutMixn to resize list titlepanes

--- list_embedded/RepeatedEmbeddedWidget.js
+++ list_embedded/RepeatedEmbeddedWidget.js
@@ -1,12 +1,13 @@
 define([ "dojo/_base/lang", "dojo/_base/array", "dojo/_base/declare",
 		"dijit/_WidgetBase", "dijit/_Container", "dijit/_TemplatedMixin",
 		"dijit/_WidgetsInTemplateMixin","dojo/Stateful","./PolymorphicMemberWidget","../Editor",
-		"dojo/text!./repeated_embedded_attribute.html", "dijit/TitlePane", "dojo/i18n!../nls/messages"
+		"dojo/text!./repeated_embedded_attribute.html", "dijit/TitlePane", "dojo/i18n!../nls/messages",
+		"../_LayoutMixin",
 ], function(lang, array, declare, _WidgetBase, _Container, _TemplatedMixin,
-		_WidgetsInTemplateMixin, Stateful,PolymorphicMemberWidget,Editor,template, TitlePane, messages) {
+		_WidgetsInTemplateMixin, Stateful,PolymorphicMemberWidget,Editor,template, TitlePane, messages, _LayoutMixin) {
 
 	return declare("app.RepeatedEmbeddedWidget", [ _WidgetBase, _Container,
-			_TemplatedMixin, _WidgetsInTemplateMixin ], {
+			_TemplatedMixin, _WidgetsInTemplateMixin, _LayoutMixin ], {
 		templateString : template,
 		messages: messages,
 		postCreate : function() {
