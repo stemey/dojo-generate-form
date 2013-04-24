db55d696e009e4d32907540ac4e7126b58eb2a71
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2013-04-24 18:47:57 +0200

    improved gridx demo

---
+++ tests/gridx/EditorController.js
@@ -0,0 +1,99 @@
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/dom-class",
+	"dojo/text!./editorschema.json",
+	"gform/Editor",	
+	"gform/createLayoutEditorFactory",	
+  "dijit/_WidgetBase", 
+	"dijit/_TemplatedMixin",
+	"dijit/_WidgetsInTemplateMixin",
+	"dojo/text!./editor.html",
+	"dijit/form/Button",
+	"dijit/Dialog"
+], function(declare, lang, domClass, editorSchema, Editor, createEditorFactory, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template){
+
+
+	
+return declare("gform.tests.gridx.EditorController", [ _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+		baseClass : "gformEditorController",
+		templateString : template,
+		store: null,
+		state:"create",
+		currentId:100,
+		postCreate : function() {	
+			this.editor.set("editorFactory",createEditorFactory());
+			this.editor.setMetaAndPlainValue(dojo.fromJson(editorSchema), {});
+			this.watch("state",lang.hitch(this,"_onStateChange"));
+			this.editor.on("value-changed",lang.hitch(this,"_onStateChange"));
+			//this.editor.on("valid-change",lang.hitch(this,"_onStateChange"));
+		},
+		_onStateChange: function(e) {
+			this.discardButton.set("disabled",!this.editor.hasChanged());
+			this.deleteButton.set("disabled",this.state=="create");
+			this.stateElement.innerHTML=this.get("state")=="create"?'New':'Edit';
+		},
+		_checkState: function(callback) {
+			if (this.state=="create" && this.editor.hasChanged()) {
+				this._startDialog("do you want to disacrd the new entity",callback);
+			} else if (this.state=="edit" && this.editor.hasChanged()) {
+				this._startDialog("do you want to disacrd the changes",callback);
+			}else{
+				callback(); 
+			}
+		},
+		edit: function(id) {
+			this._checkState(lang.hitch(this,"_edit", id));
+		},
+		discard: function() {
+			this.editor.reset();			
+		},
+		_edit: function(id) {
+			this.set("state","edit");
+			var entity = this.store.get(id);
+			this.editor.set("plainValue", entity);
+		},
+		createNew: function() {
+			if (this.state=="create") {
+				this.editor.reset();
+			}	else {
+				this._checkState(lang.hitch(this,"_createNew"));
+			}
+		},
+		_createNew: function() {
+			this.set("state","create");
+			this.editor.set("plainValue", {id:this.currentId++});
+		},
+		save: function() {
+			var entity = this.editor.get("plainValue");
+			if (this.state==	"create") {
+				this.store.add(entity);
+				this.set("state","edit");
+			}else{
+				this.store.put(entity);
+			}
+			// reset editor to remove change indicators
+			this.editor.set("plainValue",entity);
+		},
+		startup: function() {
+			this.inherited(arguments);
+			this._onStateChange();
+		},
+		_startDialog: function(message,callback) {
+			this.dialogMessage.innerHTML=message;
+			this.dialogYesButton.on("click",callback);
+			var dialog=this.dialog;
+			this.dialogYesButton.on("click",function() {dialog.hide();});
+			this.dialogNoButton.on("click",function() {dialog.hide();});
+			this.dialog.show();
+		},
+		remove: function() {
+			if (this.state!="create") {
+				var entity = this.editor.get("plainValue");
+				this.store.remove(entity.id);
+			}
+		}
+	});
+
+
+});
--- tests/gridx/GridController.js
+++ tests/gridx/GridController.js
@@ -1,22 +1,31 @@
 define([
 	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/aspect",
 	"gridx/Grid",
 	'gridx/core/model/cache/Sync',
 	"gridx/modules/VirtualVScroller",
 	"gridx/modules/ColumnResizer",
 	"gridx/modules/SingleSort",
 	"gridx/modules/Filter",
+	'gridx/modules/Focus',
+	'gridx/modules/RowHeader',
+	'gridx/modules/select/Row',
 	"dojo/store/Memory",
 	"dojo/json",
 	"dojo/text!./tablestructure.json",
 	"dojo/text!./table.json",
+	"./EditorController",
   "dijit/_WidgetBase", 
 	"dijit/_TemplatedMixin",
 	"dijit/_WidgetsInTemplateMixin",
-	"dojo/text!./grid.html"
-], function(declare, Grid, Cache, 
-	VirtualVScroller, ColumnResizer, SingleSort, 
-	Filter, Store, json, tableStructure, tabledata, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template){
+	"dojo/text!./grid.html",
+	"dijit/layout/BorderContainer",
+	"dijit/layout/ContentPane",
+	"dijit/Toolbar"
+], function(declare, lang, aspect, Grid, Cache, 
+	VirtualVScroller, ColumnResizer, SingleSort, Filter, Focus, RowHeader, RowSelect,
+	 Store, json, tableStructure, tabledata, EditorController, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template){
 
 
 	
@@ -27,18 +36,59 @@ return declare("gform.tests.gridx.GridController", [ _WidgetBase, _TemplatedMixi
 			var props={ id: "grid"};
 			props.cacheClass=Cache;
 			props.structure = dojo.fromJson(tableStructure);
-			props.store = new Store({data:dojo.fromJson(tabledata)});
+			this.store = new Store({idProperty: 'id', data:dojo.fromJson(tabledata)});
+			props.store = this.store;
 			props.modules= [
-				VirtualVScroller
+				VirtualVScroller,
+				{
+					moduleClass: RowSelect,
+					multiple: false,
+					triggerOnCell: true,
+				},
+				RowHeader
 			];
 			this.grid = new Grid(props);
 			window.grid=this.grid;
 		},
 		startup: function() {
 			this.inherited(arguments);
-			this.grid.placeAt(this.gridContainer);
-			this.grid.startup();
-		}
+			this.grid.select.row.connect(this.grid.select.row, "onSelected",lang.hitch(this,"rowSelected"));
+			this.gridContainer.addChild(this.grid);
+			this.borderContainer.layout();
+			this.editorController.set("store", this.store);
+			aspect.before(this.store,"remove",lang.hitch(this,"_onDelete"));
+		},
+		rowSelected: function(e) {
+			this.editorController.edit(e.id);
+		},
+		createNew: function() {
+			this.editorController.createNew();
+		},
+		previous: function() {
+			this._moveSelection(-1);
+		},
+		next: function() {
+			this._moveSelection(1);
+		},
+		_onDelete: function() {
+			this._moveSelection(1);
+		},
+		_moveSelection: function(delta) {
+			var selectedId = this.grid.select.row.getSelected();
+			if (selectedId) {
+				var index = this.grid.model.idToIndex(selectedId);
+				if (index<0) {
+					return;
+				}
+				var nextId = this.grid.model.indexToId(index+delta);
+				if (typeof nextId == "undefined") {
+					return;
+				}else{
+					this.grid.select.row.deselectById(selectedId);
+					this.grid.select.row.selectById(nextId);
+				}
+			}
+		}	
 	});
 
 
