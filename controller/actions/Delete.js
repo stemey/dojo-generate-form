d277f11ffc47e55e1f97dd47fc3a03d9e64ff30d
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-02-18 07:57:07 +0100

    use idProperty from store instead of „id“

--- controller/actions/Delete.js
+++ controller/actions/Delete.js
@@ -1,37 +1,38 @@
 define([
-	"dojo/_base/declare",
-	"dojo/_base/lang",
+    "dojo/_base/declare",
+    "dojo/_base/lang",
     "dojo/when",
-	"./_ActionMixin",
-	"dojo/i18n!../../nls/messages"
+    "./_ActionMixin",
+    "dojo/i18n!../../nls/messages"
 
-], function(declare, lang, when, _ActionMixin, messages	){
+], function (declare, lang, when, _ActionMixin, messages) {
 // module:
 //		gform/controller/actions/Delete
 
-	
-	return declare( [_ActionMixin], {
-		// summary:
-		//		Deletes the entity. Afterwards the editor is cleared and can be used to create a new entity.
-		messageModule: "actions.delete",
-		execute: function() {
-			if (this.state!="create") {
-				var entity = this.ctrl.editor.get("plainValue");
-				this.ctrl.editor.reset();
-				this.ctrl._removeChangeIndicator();
-				this.ctrl.showProgressBar("deleting "+this.ctrl.editor.getLabel());	
-				when(this.ctrl.store.remove(entity.id))
-					.then(lang.hitch(this,"_onRemoved"))
-					.otherwise(lang.hitch(this,"_onRemoveFailed"));
-			}
-		},
-		_onRemoved: function() {
-			this.ctrl.editor.setPlainValue({});
-			this.ctrl.set("state","create");
-		},
-		_onRemoveFailed: function() {
-			this.ctrl.set("state","edit");
-			this.ctrl.alert(messages["actions.delete.serverError"]);
-		}
-	});
+
+    return declare([_ActionMixin], {
+        // summary:
+        //		Deletes the entity. Afterwards the editor is cleared and can be used to create a new entity.
+        messageModule: "actions.delete",
+        execute: function () {
+            if (this.state != "create") {
+                var entity = this.ctrl.editor.get("plainValue");
+                this.ctrl.editor.reset();
+                this.ctrl._removeChangeIndicator();
+                this.ctrl.showProgressBar("deleting " + this.ctrl.editor.getLabel());
+                var idProperty = this.ctrl.store.idProperty;
+                when(this.ctrl.store.remove(entity[idProperty]))
+                    .then(lang.hitch(this, "_onRemoved"))
+                    .otherwise(lang.hitch(this, "_onRemoveFailed"));
+            }
+        },
+        _onRemoved: function () {
+            this.ctrl.editor.setPlainValue({});
+            this.ctrl.set("state", "create");
+        },
+        _onRemoveFailed: function () {
+            this.ctrl.set("state", "edit");
+            this.ctrl.alert(messages["actions.delete.serverError"]);
+        }
+    });
 });
