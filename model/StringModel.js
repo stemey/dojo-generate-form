ddcc28e03d4e0791c48e4b432a87285b06872664
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-03-21 17:17:51 +0100

    removed superfluous method

--- model/StringModel.js
+++ model/StringModel.js
@@ -7,17 +7,10 @@ define([
 	return declare([PrimitiveModel], {
 		code: "string",
 		isEmpty: function () {
-//			return this.value === null || this.value.length === 0 || this.value.match(/ +/) !== null;
 			return (!this.value || /^\s*$/.test(this.value));
 		},
 		isInstance: function (value) {
 			return typeof value === "string";
-		},
-		updateXX: function (value) {
-			if (value == null) {
-				value = "";
-			}
-			this._changeAttrValue("value", value);
 		}
 	});
 
