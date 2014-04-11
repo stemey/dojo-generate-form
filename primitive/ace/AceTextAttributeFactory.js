92be9ab1149365252266d4eb5b1bdfeae616193b
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-04-11 06:55:03 +0200

    added autocomplete support to ace

--- primitive/ace/AceTextAttributeFactory.js
+++ primitive/ace/AceTextAttributeFactory.js
@@ -5,7 +5,8 @@ define([
     "dojo/_base/declare",
     "../../schema/meta",
     "dojox/mvc/at",
-    "../PrimitiveAttributeFactory"
+    "../PrimitiveAttributeFactory",
+    'ace/ext/language_tools'
 ], function (lang, aspect, AceEditor, declare, meta, at, PrimitiveAttributeFactory) {
 
     return declare([PrimitiveAttributeFactory], {
@@ -18,6 +19,8 @@ define([
             var props = {};
             props.value = at(modelHandle, "value");
             props.mode = attribute.mode;
+            props.options={};
+            props.options.enableBasicAutocompletion=!!attribute.autoComplete;
             var widget = new AceEditor(props);
             aspect.after(widget, "startup", lang.hitch(this, "startup", widget, attribute));
             return widget;
