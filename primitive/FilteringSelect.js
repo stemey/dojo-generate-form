c99ec728b09b0e97b20927cd55777b2cf7ed0e62
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-04-19 07:41:32 +0200

    revert fix to „set null does not work“

--- primitive/FilteringSelect.js
+++ primitive/FilteringSelect.js
@@ -12,25 +12,6 @@ define([
         //		Base class for date textbox widgets with the ability to validate content of various types and provide user feedback.
         //	templateString: template,
         displayMessage: function (/*String*/ message) {
-        },
-        _callbackSetLabel: function (/*Array*/ result, /*Object*/ query, /*Object*/ options, /*Boolean?*/ priorityChange) {
-            // summary:
-            //		Callback from dojo.store after lookup of user entered value finishes
-
-            // setValue does a synchronous lookup,
-            // so it calls _callbackSetLabel directly,
-            // and so does not pass dataObject
-            // still need to test against _lastQuery in case it came too late
-            if ((query && query[this.searchAttr] !== this._lastQuery) || (!query && result.length && this.store.getIdentity(result[0]) != this._lastQuery)) {
-                return;
-            }
-            if (!result.length || result.length > 1) {
-                //#3268: don't modify display value on bad input
-                //#3285: change CSS to indicate error
-                this.set("value", '', priorityChange || (priorityChange === undefined && !this.focused), this.textbox.value, null);
-            } else {
-                this.set('item', result[0], priorityChange);
-            }
         }
     });
 
