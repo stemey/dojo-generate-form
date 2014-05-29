edc84501f1a355e10f86996d772ec8b20135ca28
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-05-29 07:55:59 +0200

    don’t validate min items  on user input

--- validate/MinItems.js
+++ validate/MinItems.js
@@ -7,10 +7,14 @@ define([
 
 
 	return function (minItems) {
-		return  function (model) {
+		return  function (model, force) {
+            if (!force) {
+                // this is rather an incomplete situation
+                return [];
+            }
 			// TODO currently only handles one unique property
 			var errors = [];
-			if (model != null && model.length() < minItems) {
+			if (model !== null && model.length() < minItems) {
 				var msg = lang.replace(nls.minItems, {minItems: minItems});
 				errors.push({path: "", message: msg});
 			}
