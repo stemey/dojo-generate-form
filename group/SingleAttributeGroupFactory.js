7f8c93f441af36d6bc7ca13148d8f135e63229b0
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-06-08 07:37:13 +0200

    single attribute resizes widget to take up all the space. Ideal to display rich text editor in tab.

---
+++ group/SingleAttributeGroupFactory.js
@@ -0,0 +1,41 @@
+define([
+    "dojo/_base/declare",
+    "./SingleDecoratorWidget",
+    "dijit/layout/BorderContainer",
+    "dojo/_base/lang",
+    "../model/SingleObject"
+
+], function (declare, SingleDecoratorWidget, BorderContainer, lang, SingleObject) {
+
+    return declare("gform.group.SingleAttributeGroupFactory", [], {
+        id: "single",
+        constructor: function (kwArgs) {
+            lang.mixin(this, kwArgs);
+        },
+        createModel: function (schema, plainValue) {
+            var normalizedSchema = {};
+            lang.mixin(normalizedSchema, schema);
+            normalizedSchema.attributes=[schema.attribute];
+            var validators = this.editorFactory.getModelValidators(normalizedSchema);
+            var model = new SingleObject({schema:normalizedSchema, validators:validators, editorFactory:this.editorFactory});
+            model.update(plainValue);
+            model.typeCode = schema.code;
+            return model;
+        },
+        create: function (group, modelHandle, ctx) {
+                var attribute = group.attribute;
+                var attributeModel = modelHandle.getModel(attribute.code);
+                var attributeFactory = this.editorFactory.getAttributeFactory(attribute, attributeModel, ctx);
+                var attributeEditor = attributeFactory.create(attribute, attributeModel, ctx);
+                var widget = new BorderContainer({class: "singleDecorator", gutters: false});
+                var decorator = new SingleDecoratorWidget({region: 'top', meta: attribute, modelHandle: modelHandle});
+                widget.addChild(decorator);
+                if (attributeEditor !== null) {
+                    attributeEditor.region = "center";
+                    widget.addChild(attributeEditor);
+                }
+                return widget;
+
+        }
+    });
+});
