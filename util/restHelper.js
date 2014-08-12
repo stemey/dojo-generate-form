971afc2bf86cd1b7414c7a22761d28f2854302af
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-08-12 11:56:19 +0200

    it should be possible for ids to contains slashes

--- util/restHelper.js
+++ util/restHelper.js
@@ -1,31 +1,37 @@
 define([
 ], function () {
 
-    return  {
-        decompose: function (url) {
-            //  description:
-            //		decompose url into id and collectionUrl
-            //  url: String
-            //		the url
-            //  returns:
-            //		an object containing the id and the url to the collection.
-            var lastSlashIndex = url.lastIndexOf("/");
-            var id = url.substring(lastSlashIndex + 1, url.length);
-            var collectionUrl = url.substring(0, lastSlashIndex);
-            return {id: id, url: collectionUrl};
-        },
-        compose: function (collectionUrl, id) {
-            //  description:
-            //		compose url to resource from id and collectionUrl
-                //  collectionUr id
-            //  returns:
-            //		the url to the resource.
-            if (collectionUrl.substring(collectionUrl.length-1)=="/") {
-                return collectionUrl +  id;
-            }else {
-                return collectionUrl + "/" + id;
-            }
-        }
-    };
+	return  {
+		decompose: function (url) {
+			//  description:
+			//		decompose url into id and collectionUrl
+			//  url: String
+			//		the url
+			//  returns:
+			//		an object containing the id and the url to the collection.
+            if (url){
+			var match = url.match(/(\/?[^\/]+)\/(.*)/);
+                    var id = match[2];
+                    var collectionUrl = match[1];
+                    return {id: id, url: collectionUrl};
+                } else {
+                    return {id:null,url:null}
+               }
+
+		},
+		compose: function (collectionUrl, id) {
+			//  description:
+			//		compose url to resource from id and collectionUrl
+			//  collectionUrl: String
+			//		the collection's url
+			//  id: any
+			//		the resoure's id
+			//  returns:
+			//		the url to the resource.
+            collectionUrl=collectionUrl.replace(/\/+$/,"")
+            return collectionUrl + "/" +encodeURIComponent(id);
+
+		}
+	};
 
 });
