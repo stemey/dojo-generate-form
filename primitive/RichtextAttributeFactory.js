bd88c473d4520a1a2eb8712fcee2d8ef82535faf
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-10-11 10:32:23 +0200

    added live model updates to rich text editor

--- primitive/RichtextAttributeFactory.js
+++ primitive/RichtextAttributeFactory.js
@@ -1,4 +1,5 @@
 define([
+    'dojox/mvc/sync',
     "dojo/aspect",
     "dojo/_base/lang",
     "dojo/_base/declare",
@@ -7,7 +8,7 @@ define([
     "./mixinTextboxBindings",
     "./dijitHelper",
     "./PrimitiveAttributeFactory"
-], function (aspect, lang, declare, Editor, meta, mixinTextboxBindings, dijitHelper, PrimitiveAttributeFactory) {
+], function (sync, aspect, lang, declare, Editor, meta, mixinTextboxBindings, dijitHelper, PrimitiveAttributeFactory) {
 
     return declare([PrimitiveAttributeFactory], {
         id: "richtext",
@@ -24,12 +25,35 @@ define([
                 props.extraPlugins = attribute.extraPlugins;
             }
             props.height = attribute.height;
-            if (modelHandle.value === null || typeof modelHandle.value === "undefined") {
-                modelHandle.value = "";
-            }
-            mixinTextboxBindings(modelHandle, props);
+
+            props.value = at(modelHandle, "message");
+            props.state = at(modelHandle, "state");
+
+            var converter = {
+                format: function (value) {
+                    if (this.target.get("focused")) {
+                        throw new "don't update when focus is set";
+                    }
+                    return value;
+                },
+                parse: function (value) {
+                    return value;
+                }
+            };
+
+
             dijitHelper.copyDijitProperties(attribute, props);
+            dijitHelper.copyProperty("updateInterval", attribute, props);
             var widget = new Editor(props);
+            var liveChanges = props.updateInterval && props.updateInterval >= 0;
+            sync(modelHandle, "value", widget, "value", {converter: converter});
+            if (liveChanges) {
+                aspect.after(widget, "onNormalizedDisplayChanged", function () {
+                    modelHandle.set("value", widget.get("value"));
+                    //console.log("changed "+widget.get("value"))
+                });
+            }
+            // TODO do we need to remove the aspects on destroy?
             aspect.after(widget, "_onBlur", lang.hitch(modelHandle, "onTouch"));
             return widget;
         }
