aa820abf66e4ce782d3e824ce540743d2e146e59
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-10-16 08:43:45 +0200

    fix #42: added disabled to boolean attribute definition

--- primitive/BooleanAttributeFactory.js
+++ primitive/BooleanAttributeFactory.js
@@ -1,37 +1,38 @@
 define([
-	"dojo/_base/declare",//
-	"dojo/aspect",//
-	"dojox/mvc/at",//
-	"dijit/form/CheckBox",//
-	"../schema/meta",//
-	"./dijitHelper",//
-	"../model/BooleanModel"
+    "dojo/_base/declare",//
+    "dojo/aspect",//
+    "dojox/mvc/at",//
+    "dijit/form/CheckBox",//
+    "../schema/meta",//
+    "./dijitHelper",//
+    "../model/BooleanModel"
 ], function (declare, aspect, at, CheckBox, meta, dijitHelper, BooleanModel) {
 
-	return declare([], {
-		id: "boolean",
-		handles: function (attribute) {
-			return meta.isType(attribute, "boolean") && !attribute.array;
-		},
-		create: function (attribute, modelHandle) {
-			if (!modelHandle) {
-				throw new Error(" attribute " + attribute.code + " was not initialized");
-			}
-			var box = new CheckBox({
-				"checked": at(modelHandle, "value")
-			});
-			// remove errors when value changes because this select does not validate.
-			aspect.after(box, "onChange", function () {
-				modelHandle.set("message", null);
-				modelHandle.set("valid", true);
-			});
-			return box;
+    return declare([], {
+        id: "boolean",
+        handles: function (attribute) {
+            return meta.isType(attribute, "boolean") && !attribute.array;
+        },
+        create: function (attribute, modelHandle) {
+            if (!modelHandle) {
+                throw new Error(" attribute " + attribute.code + " was not initialized");
+            }
+            var box = new CheckBox({
+                "checked": at(modelHandle, "value"),
+                "disabled": attribute.disabled === true
+            });
+            // remove errors when value changes because this select does not validate.
+            aspect.after(box, "onChange", function () {
+                modelHandle.set("message", null);
+                modelHandle.set("valid", true);
+            });
+            return box;
 
-		},
-		createModel: function (meta, plainValue) {
-			var model = new BooleanModel({schema:meta});
-			model.update(plainValue);
-			return model;
-		}
-	});
+        },
+        createModel: function (meta, plainValue) {
+            var model = new BooleanModel({schema: meta});
+            model.update(plainValue);
+            return model;
+        }
+    });
 });
