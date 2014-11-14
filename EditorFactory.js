7571b921c282d5f12135d0379640e4b08c59248e
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-11-14 07:35:39 +0100

    initialize Maps in constructor

--- EditorFactory.js
+++ EditorFactory.js
@@ -2,10 +2,10 @@ define([  //
 	"dojo/_base/declare",//
 	"dojo/Stateful", //
 	"./converter/urlToIdConverter", //
-    "./converter/anyToTextConverter", //
+	"./converter/anyToTextConverter", //
 	"./group/DecoratorFactory", //
 	"./validate/UniqueProperties",//
-    "./validate/AdditionalProperties",//
+	"./validate/AdditionalProperties",//
 	"./validate/Min",//
 	"./validate/Max",//
 	"./validate/MinItems",//
@@ -14,7 +14,7 @@ define([  //
 	"./validate/MaxLength",//
 	"./validate/Pattern"//
 ], function (declare, Stateful, urlToIdConverter, anyToTextConverter, DecoratorFactory, UniqueProperties, AdditionalProperties, Min, Max, MinItems, MaxItems, MinLength, MaxLength, Pattern) {
-	// module: 
+	// module:
 	//		gform/EditorFactory
 
 	return declare("gform.EditorFactory", [Stateful], {
@@ -24,36 +24,38 @@ define([  //
 		// asyncModelValidation: boolean
 		//		dijits validate after setting the value. Therefore model validation must happen asynchronuously
 		//		to value change events.
-		convertersById: {},
-		convertersByType: {},
+		convertersById: null,
+		convertersByType: null,
 		constructor: function () {
 			this.groupFactories = {};
+			this.convertersById = {};
+			this.convertersByType = {};
 			this.decoratorFactory = new DecoratorFactory();
 			this.addConverterForType(urlToIdConverter, "ref");
-            this.addConverterForType(urlToIdConverter, "multi-ref");
-            this.addConverterForType(anyToTextConverter, "any");
+			this.addConverterForType(urlToIdConverter, "multi-ref");
+			this.addConverterForType(anyToTextConverter, "any");
 
-            this.factoryValidators={
-                uniqueProperties: UniqueProperties,
-                    pattern: Pattern,
-                minLength: MinLength,
-                maxLength: MaxLength,
-                min: Min,
-                max: Max,
-                minItems: MinItems,
-                maxItems: MaxItems,
-                additionalProperties:AdditionalProperties
-            }
-            this.constructorValidators={};
-        },
-        createBadge: function(model) {
-            return this.decoratorFactory.createBadge(model);
-        },
+			this.factoryValidators = {
+				uniqueProperties: UniqueProperties,
+				pattern: Pattern,
+				minLength: MinLength,
+				maxLength: MaxLength,
+				min: Min,
+				max: Max,
+				minItems: MinItems,
+				maxItems: MaxItems,
+				additionalProperties: AdditionalProperties
+			}
+			this.constructorValidators = {};
+		},
+		createBadge: function (model) {
+			return this.decoratorFactory.createBadge(model);
+		},
 		createGroupModel: function (schema, plainValue) {
 			var factory;
-            if (typeof plainValue === "undefined") {
-                plainValue=null;
-            }
+			if (typeof plainValue === "undefined") {
+				plainValue = null;
+			}
 			if (schema.editor) {
 				factory = this.getGroupFactory(schema.editor);
 			} else {
@@ -62,7 +64,7 @@ define([  //
 			if (factory === null) {
 				throw new Error("cannot find group factory for type " + schema.editor);
 			}
-			return factory.createModel(schema, plainValue );
+			return factory.createModel(schema, plainValue);
 		},
 		createAttributeModel: function (attribute, plainValue) {
 			var factory = this.getAttributeFactory(attribute);
@@ -102,12 +104,12 @@ define([  //
 		createDecorator: function (/*Object*/attribute, /*dojo.Stateful*/modelHandle, decorator) {
 			// summary:
 			//		creates a Decorator Widget for the given attribute. The Decorator displays the label, description and state of the attribute. The actual widget for editing the attribute's value is added as a child.
-			// attribute: Object			
+			// attribute: Object
 			//		The attribute schema.
-			// modelHandle: dojo/Stateful			
+			// modelHandle: dojo/Stateful
 			//		The modelHandle. The Decorator watches the state and value of the attribute via its modelHandle meta data.
 			// returns: dijit/Widget
-			//		The Widget ich contains all widgets for the attributes and groups contained in the given attribute schema.	
+			//		The Widget ich contains all widgets for the attributes and groups contained in the given attribute schema.
 			return this.decoratorFactory.create(/*Object*/attribute, /*dojo/stateful*/modelHandle, decorator);
 		},
 		create: function (group, modelHandle, ctx) {
@@ -175,18 +177,18 @@ define([  //
 			// returns: Object
 			return this.attributeFactoryFinder.getAttributeFactoryMap();
 		},
-        addAttributeFactory: function (af) {
-            // summary:
-            //		the AttributeFactory
-            // returns: id
-            return this.attributeFactoryFinder.addAttributeFactory(af);
-        },
-        getAttributeFactoryById: function (id) {
-            // summary:
-            //		the AttributeFactory
-            // returns: id
-            return this.attributeFactoryFinder.getAttributeFactoryMap()[id];
-        },
+		addAttributeFactory: function (af) {
+			// summary:
+			//		the AttributeFactory
+			// returns: id
+			return this.attributeFactoryFinder.addAttributeFactory(af);
+		},
+		getAttributeFactoryById: function (id) {
+			// summary:
+			//		the AttributeFactory
+			// returns: id
+			return this.attributeFactoryFinder.getAttributeFactoryMap()[id];
+		},
 		getModelValidators: function (attribute) {
 			// summary:
 			//		attach validation to modelHandle.
@@ -196,20 +198,20 @@ define([  //
 				if (attribute[key]) {
 					var validator = this.factoryValidators[key];
 					if (validator) {
-                        validators.push(validator(attribute[key]));
+						validators.push(validator(attribute[key]));
 
 					}
 				}
 			}
-            for (var key in  this.constructorValidators) {
-                if (attribute[key]) {
-                    var validator = this.constructorValidators[key];
-                    if (validator) {
-                        validators.push(new validator(attribute[key]));
+			for (var key in  this.constructorValidators) {
+				if (attribute[key]) {
+					var validator = this.constructorValidators[key];
+					if (validator) {
+						validators.push(new validator(attribute[key]));
 
-                    }
-                }
-            }
+					}
+				}
+			}
 			return validators;
 		},
 		createValidateFunction: function (validator) {
@@ -228,13 +230,13 @@ define([  //
 			return validateFn;
 		},
 		factoryValidators: null,
-        constructorValidators: null,
+		constructorValidators: null,
 		// summary:
 		//		get a converter for the given attribute.
 		// attribute: object
 		//		the attribute meta data. the type property is usually used to lookup a converter
 		// ctx: gform/Context
-		//		the context provides extra information (e.g. the storeRegistry that can translate from store ids to actual urls).		
+		//		the context provides extra information (e.g. the storeRegistry that can translate from store ids to actual urls).
 		getConverter: function (attribute, ctx) {
 			var c;
 			if (attribute.converter) {
@@ -251,28 +253,28 @@ define([  //
 		},
 		// summary:
 		//		add a converter for a specific type
-		// converter: 
-		//		The converter may be an instance or a constructor. constructor will be called with the parameters attribute and ctx.	
+		// converter:
+		//		The converter may be an instance or a constructor. constructor will be called with the parameters attribute and ctx.
 		// type: string
-		//		type (e.g. "string", "ref")	
+		//		type (e.g. "string", "ref")
 		addConverterForType: function (converter, type) {
 			this.convertersByType[type] = converter;
 		},
 		// summary:
 		//		add a converter for a specific id (ids an be used in the schema).
-		// converter: 
-		//		The converter may be an instance or a constructor. constructor will be called with the parameters attribute and ctx.	
+		// converter:
+		//		The converter may be an instance or a constructor. constructor will be called with the parameters attribute and ctx.
 		// id: string
 		//		the id is matched against the converter property in the attribute meta data.
 		addConverterForid: function (converter, id) {
 			this.convertersById[id] = converter;
 		},
-        addValidator: function(id, validator) {
-            this.factoryValidators[id]=validator;
-        },
-        addCtrValidator: function(id, validator) {
-            this.constructorValidators[id]=validator;
-        }
+		addValidator: function (id, validator) {
+			this.factoryValidators[id] = validator;
+		},
+		addCtrValidator: function (id, validator) {
+			this.constructorValidators[id] = validator;
+		}
 
 	});
 });
