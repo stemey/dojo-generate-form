a76c18e608dde1133a4469c4372d9119b7a155d0
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2014-11-16 06:54:44 +0100

    required needs to be false

--- embedded/OptionalSingleTypePanelWidget.js
+++ embedded/OptionalSingleTypePanelWidget.js
@@ -1,18 +1,19 @@
 define([
-    "dojo/_base/declare", "./SingleTypePanelWidget", "dojo/text!./singletype_embedded_attribute.html",
-    "../group/_DecoratorMixin", "dijit/form/CheckBox"
+	"dojo/_base/declare", "./SingleTypePanelWidget", "dojo/text!./singletype_embedded_attribute.html",
+	"../group/_DecoratorMixin", "dijit/form/CheckBox"
 ], function (declare, SingleTypePanelWidget, template, DecoratorMixin) {
 
-    return declare([ SingleTypePanelWidget, DecoratorMixin ], {
+	return declare([SingleTypePanelWidget, DecoratorMixin], {
 
-        templateString: null,
-        doLayout: true,
-        required: false,
-        constructor: function (config) {
-            this.label = config.meta && (config.meta.label || config.meta.code) || "";
-            this.templateString = template;
-        }
+		templateString: null,
+		doLayout: true,
+		required: false,
+		constructor: function (config) {
+			this.label = config.meta && (config.meta.label || config.meta.code) || "";
+			this.templateString = template;
+			this.required = false;
+		}
 
-    });
+	});
 
 });
