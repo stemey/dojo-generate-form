d0f9f40823e03b39b7bd440948d7392ddba0365d
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2015-05-01 07:19:26 +0200

    fix setting it to empty

--- primitive/FilteringSelect.js
+++ primitive/FilteringSelect.js
@@ -1,26 +1,25 @@
 define([
-    "dojo/_base/declare",
-    "dijit/form/FilteringSelect",
-    "./_NotValidatingOnFocusMixin"
+	"dojo/_base/declare",
+	"dijit/form/FilteringSelect",
+	"./_NotValidatingOnFocusMixin"
 ], function (declare, FilteringSelect, _NotValidatingOnFocusMixin) {
 
-    // module:
-    //		dijit/form/DateTextBox
+	// module:
+	//		dijit/form/DateTextBox
 
-    return declare("gform.FilteringSelect", [FilteringSelect, _NotValidatingOnFocusMixin], {
-        // summary:
-        //		Base class for date textbox widgets with the ability to validate content of various types and provide user feedback.
-        //	templateString: template,
-        displayMessage: function (/*String*/ message) {
-        },
-		_onBlur: function() {
-			this.inherited(arguments);
-			if (!this.displayedValue) {
-				//value, /*Boolean?*/ priorityChange, /*String?*/ displayedValue, /*item?*/ item
-				// only way to set the value to null
-				this.set("value",null, null,"",{});
+	return declare("gform.FilteringSelect", [FilteringSelect, _NotValidatingOnFocusMixin], {
+		// summary:
+		//		Base class for date textbox widgets with the ability to validate content of various types and provide user feedback.
+		//	templateString: template,
+		displayMessage: function (/*String*/ message) {
+		},
+		_setDisplayedValueAttr: function (/*String*/ label, /*Boolean?*/ priorityChange) {
+			if (this.store && this._created && label==="") {
+				this.set("value",null, null, "", {});
+				return;
+			} else{
+				this.inherited(arguments);
 			}
 		}
-    });
-
+	});
 });
